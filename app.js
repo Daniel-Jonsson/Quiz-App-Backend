2a2878f9241e0a23377a398d36b96b84530a2e9d
Daniel Jönsson <dajo1903@student.miun.se>
2023-12-19 12:13:25 +0100

    Added routes/models

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require("express");
 const cors = require("cors");
 const connectDB = require('./database');
+const subjectRoute = require('./routes/subjects');
 
 
 
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 // Routes
+app.use(`${BASE_API_URL}/subjects`, subjectRoute);
 
 connectDB();
 
