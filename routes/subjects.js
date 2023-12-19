72314c343087545bab8d96c1d6d368304837a53a
Daniel Jönsson <dajo1903@student.miun.se>
2023-12-19 15:35:38 +0100

    Added CRUD functionality

--- routes/subjects.js
+++ routes/subjects.js
@@ -20,6 +20,33 @@ subjectRoute.get("/:subjectCode", (req, res) => {
 		.catch((err) => console.error(err));
 });
 
+subjectRoute.post("/", (req, res) => {
+	subjectModel
+		.addSubject(req.body)
+		.then((response) => {
+			console.log(response);
+			res.status(201).json(response);
+		})
+		.catch((err) => console.log(err));
+});
 
+subjectRoute.delete("/:subjectCode", (req, res) => {
+	subjectModel
+		.deleteSubject(req.params.subjectCode)
+		.then((result) => {
+			res.status(200).json(result);
+		})
+		.catch((err) => console.log(err));
+});
+
+subjectRoute.put("/:subjectCode", (req, res) => {
+	const subjectCode = req.params.subjectCode;
+	const updatedSubject = req.body;
+	subjectModel.updateSubject(subjectCode, updatedSubject)
+    .then((updatedSubject) => {
+        res.status(200).json(updatedSubject)
+    })
+    .catch((err) => console.log(err))
+});
 
 module.exports = subjectRoute;
