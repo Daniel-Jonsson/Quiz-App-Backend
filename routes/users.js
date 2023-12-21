f3c2273dea6e90812987333b438cfea8fd0579e9
Daniel Jönsson <dajo1903@student.miun.se>
2023-12-21 13:51:15 +0100

    Added logout feature

--- routes/users.js
+++ routes/users.js
@@ -3,15 +3,24 @@ const userRoute = express.Router();
 const userModel = require("../models/users");
 const bcrypt = require("bcrypt");
 
+userRoute.get("/logout", (req, res) => {
+	req.session.destroy((err) => {
+		if (err) {
+			res.status(500).json({ message: "Internal Server Error." });
+		} else {
+			res.status(200).json({message: "Successfully logged out."});
+		}
+	});
+});
 userRoute.post("/login", async (req, res) => {
 	const { userName, password } = req.body;
 
 	if (userName && password) {
 		const foundUser = await userModel.getUser(userName);
-        if(!foundUser) {
-            res.status(404).json({message: "User not found."});
-            return;
-        }
+		if (!foundUser) {
+			res.status(404).json({ message: "User not found." });
+			return;
+		}
 
 		const match = await bcrypt.compare(password, foundUser.password);
 		if (!match) {
@@ -42,13 +51,14 @@ userRoute.post("/signup", (req, res) => {
 });
 
 userRoute.put("/:userName", (req, res) => {
-    userModel.updateUser(req.params.userName, req.body)
-    .then((result) => {
-        res.status(200).json(result);
-    })
-    .catch((err) => {
-        console.log(err)
-    })
-})
+	userModel
+		.updateUser(req.params.userName, req.body)
+		.then((result) => {
+			res.status(200).json(result);
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+});
 
 module.exports = userRoute;
